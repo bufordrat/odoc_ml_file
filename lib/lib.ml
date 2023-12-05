b98b8bfc99a5a3ca24fdd2e8d0521a1345d40573
Matt Teichman <teichman@uchicago.edu>
2023-12-05 12:59:23 -0600

    add readme

--- lib/lib.ml
+++ lib/lib.ml
@@ -1,1 +1,5 @@
-let message = "GOODBYE CRUEL WORLD (is underrated)"
+(** Lib is a module that exists only to contain {!message}. *)
+
+let message = "help me Yawar, you are my only hope"
+(** [message] is a desperate cry for help from someone who wants to
+   attach a docstring to an .ml file *)
